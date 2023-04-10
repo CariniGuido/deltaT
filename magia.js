50cfb08570fdef6b5b9f05ed7c067218459166c6
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-04-10 16:36:02 -0300

    listo

---
+++ magia.js
@@ -0,0 +1,58 @@
+// Inicializar los diales
+var temperaturaGauge = new JustGage({
+    id: "temperatura-gauge",
+    value: 0,
+    min: -20,
+    max: 40,
+    title: "Temperatura (°C)"
+  });
+  
+  var vientoGauge = new JustGage({
+    id: "viento-gauge",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "Velocidad del viento (km/h)"
+  });
+  
+  var humedadGauge = new JustGage({
+    id: "humedad-gauge",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "Humedad (%)"
+  });
+  
+  // Función para actualizar las mediciones
+  function actualizarMediciones() {
+    // Obtener la ubicación del usuario
+    navigator.geolocation.getCurrentPosition(posicion => {
+      const latitud = posicion.coords.latitude;
+      const longitud = posicion.coords.longitude;
+  
+      // Obtener los datos meteorológicos actuales
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitud}&lon=${longitud}&exclude=minutely,hourly,daily&units=metric&appid=1421faa6896fd18ab715b2a75d3f01a1`)
+      
+        // Actualizar los valores de los diales con las mediciones
+        .then(response => response.json())
+        .then(data => {
+          // Obtener los valores de temperatura, viento y humedad
+          const temperaturaActual = data.current.temp;
+          const vientoActual = data.current.wind_speed;
+          const humedadActual = data.current.humidity;
+      
+          // Actualizar los valores de los diales con las mediciones
+          temperaturaGauge.set(temperaturaActual);
+          document.getElementById("temperatura-gauge").innerHTML = temperaturaActual.toFixed(1) + " °C";
+      
+          vientoGauge.set(vientoActual);
+          document.getElementById("viento-gauge").innerHTML = vientoActual.toFixed(1) + " km/h";
+      
+          humedadGauge.set(humedadActual);
+          document.getElementById("humedad-gauge").innerHTML = humedadActual.toFixed(1) + " %";
+        });
+    });
+  }
+
+  // Llamar a la función para actualizar las mediciones cada 50 segundos
+  setInterval(actualizarMediciones, 50000);
