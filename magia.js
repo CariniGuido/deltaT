3d28107969fa8496ae7a81d71b99aef1b20741dd
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-04-13 03:10:22 -0300

    listo

--- magia.js
+++ magia.js
@@ -1,4 +1,26 @@
+function crearRayitasTiempo() {
+    var rayitasTiempo = document.querySelector('.rayitas-tiempo');
+    var angulo = 0;
+    var incrementoAngulo = 30; // 360 grados dividido en 12 rayitas
+    for (var i = 0; i < 12; i++) {
+        var rayita = document.createElement('div');
+        rayita.classList.add('rayita-tiempo');
+        rayita.style.transform = 'rotate(' + angulo + 'deg)';
+        angulo += incrementoAngulo;
+        rayitasTiempo.appendChild(rayita);
+    }
+}
+
+crearRayitasTiempo();
+
 // Inicializar los diales
+function obtenerDireccionBrujula(angulo) {
+    const direccion = [
+        "Norte", "Este", "Sur", "Oeste"
+    ];
+    const indice = Math.round((angulo % 360) / 90);
+    return direccion[indice];
+}
 var temperaturaGauge = new JustGage({
     id: "temperatura-gauge",
     value: 0,
@@ -40,17 +62,26 @@ function actualizarMediciones() {
                 const temperaturaActual = data.current.temp;
                 const vientoActual = data.current.wind_speed;
                 const humedadActual = data.current.humidity;
-               // const deltaT = (temperaturaActual - 10) / 10 + (100 - humedadActual) / 10 + vientoActual / 10;
+                // const deltaT = (temperaturaActual - 10) / 10 + (100 - humedadActual) / 10 + vientoActual / 10;
+                const direccionVientoGrados = data.current.wind_deg;
 
+
+
+                const direccionVientoTexto = document.getElementById("direccion-viento");
+                direccionVientoTexto.textContent = obtenerDireccionBrujula(direccionVientoGrados);
+                // Calcular la dirección de la brújula correspondiente
+               
                 // Actualizar los valores de los diales con las mediciones
                 temperaturaGauge.refresh(temperaturaActual);
                 vientoGauge.refresh(vientoActual);
                 humedadGauge.refresh(humedadActual);
-
+                const agujaViento = document.getElementById("aguja-viento");
+                agujaViento.style.transform = `rotate(${360 - direccionVientoGrados}deg)`;
+            })
+            .catch(error => {
+                console.error("Error al obtener los datos meteorológicos", error);
             });
-            
     });
 }
-
-// Llamar a la función para actualizar las mediciones cada 50 segundos
-setInterval(actualizarMediciones, 500);
+setTimeout(actualizarMediciones, 0);
+setInterval(actualizarMediciones, 3600000);
