ce3fda20061a93364d853ca9faf01ad2d383eee8
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-04-13 04:20:37 -0300

    listoo

--- magia.js
+++ magia.js
@@ -1,3 +1,51 @@
+function generarGrafico(temperaturas, humedades, deltas) {
+    // Obtener el elemento canvas donde se mostrará el gráfico
+    const canvas = document.getElementById('grafico');
+  
+    // Crear el objeto Chart y configurarlo
+    const chart = new Chart(canvas, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Delta T vs. Temperatura y Humedad',
+          data: temperaturas.map((t, i) => ({x: t, y: humedades[i], r: 5, delta: deltas[i]})),
+          backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          borderColor: 'rgba(0, 0, 0, 0.5)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Temperatura (°C)'
+            },
+            min: 0,
+            max: 40
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Humedad Relativa (%)'
+            },
+            min: 0,
+            max: 100
+          }
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: function(context) {
+                return `Delta T: ${context.dataset.data[context.dataIndex].delta.toFixed(1)}`;
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+
 function crearRayitasTiempo() {
     var rayitasTiempo = document.querySelector('.rayitas-tiempo');
     var angulo = 0;
@@ -51,6 +99,9 @@ function actualizarMediciones() {
     navigator.geolocation.getCurrentPosition(posicion => {
         const latitud = posicion.coords.latitude;
         const longitud = posicion.coords.longitude;
+        let temperaturas = [];
+let humedades = [];
+let deltas = [];
 
         // Obtener los datos meteorológicos actuales
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitud}&lon=${longitud}&exclude=minutely,hourly,daily&units=metric&appid=1421faa6896fd18ab715b2a75d3f01a1`)
@@ -64,9 +115,47 @@ function actualizarMediciones() {
                 const humedadActual = data.current.humidity;
                 // const deltaT = (temperaturaActual - 10) / 10 + (100 - humedadActual) / 10 + vientoActual / 10;
                 const direccionVientoGrados = data.current.wind_deg;
+                // Calcular Delta T
+    const deltaT = (temperaturaActual - 10) / 10 + (100 - humedadActual) / 10 + vientoActual / 10;
+    deltas.push(deltaT);
+    temperaturas.push(temperaturaActual);
+    humedades.push(humedadActual);
+    let estadoDeltaT;
 
+    // Comprobar si Delta T está en el rango óptimo
+    if (deltaT >= 2 && deltaT <= 8) {
+        estadoDeltaT = "óptimo para pulverización";
+    } else if (deltaT < 2 || deltaT > 10) {
+        estadoDeltaT = "fuera del rango óptimo para pulverización";
+    }
+    
+    document.getElementById("estadodelta").innerHTML = `El estado de Delta T es ${estadoDeltaT}.`;
+    // Crear gráfico de Delta T con temperatura y humedad relativa
+    const chartData = {
+        labels: temperaturas,
+        datasets: [{
+            label: 'Delta T',
+            data: deltas,
+            borderColor: 'blue',
+            fill: false
+        }]
+    };
+    const chartOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    };
+    const chart = new Chart(document.getElementById('chart'), {
+        type: 'line',
+        data: chartData,
+        options: chartOptions
+    });
 
-
+    generarGrafico(temperaturas, humedades, deltas);
                 const direccionVientoTexto = document.getElementById("direccion-viento");
                 direccionVientoTexto.textContent = obtenerDireccionBrujula(direccionVientoGrados);
                 // Calcular la dirección de la brújula correspondiente
